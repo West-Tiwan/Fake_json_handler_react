141aa357622e86da540502b3e16780776b2a9147
West-Tiwan <xyzmohit.singh@gmail.com>
2024-03-09 01:11:56 +0530

    react toastify

--- app/page.js
+++ app/page.js
@@ -2,6 +2,9 @@
 import React, {useEffect, useState} from 'react'
 import axios from "axios";
 import Page from "@/app/[id]/page";
+import { ToastContainer, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 const index = () => {
   const [user, setUser] = useState([]);
@@ -13,11 +16,25 @@ const index = () => {
     useEffect(() => {
         getdata()
     }, []);
-
+const notify = ()=>{toast.success('Data received', {
+    position: "bottom-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+});}
+    function  clicky(){
+    getdata();
+    notify();
+    }
     return (
       <>
           <div className={"flex justify-center items-center m-4"}>
-              <button onClick={getdata} className={"border-2 border-blue-300"}>Get Data</button>
+              <button onClick={clicky} className={"border-2 border-blue-300"}>Get Data</button>
+              <ToastContainer/>
           </div>
           <div className={"flex justify-center items-center"}>
           <ol className={"list-decimal"}>{user.map((e, index) => {
