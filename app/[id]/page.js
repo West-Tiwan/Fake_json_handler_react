41e1e6c206e8eaf0fa8ed103101bb0609949353f
West-Tiwan <xyzmohit.singh@gmail.com>
2024-03-09 01:21:52 +0530

    toastify not working on 2nd page

--- app/[id]/page.js
+++ app/[id]/page.js
@@ -1,6 +1,19 @@
 "use client"
 import React, {useEffect, useState} from 'react'
 import axios from "axios";
+import { ToastContainer, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+function notify(){ toast.warn('Personal Data', {
+    position: "bottom-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+});}
 const Page = ({params}) => {
     const  {id} = params
     const [user, setUser] = useState("");
@@ -13,7 +26,7 @@ const Page = ({params}) => {
         getdata()
     }, []);
     return (
-        <>
+        <div onLoad={notify}>
             <ul className={"flex flex-col justify-center items-center min-h-44"}>
             <li>ID : {user.id}</li>
             <li>Username : {user.username}</li>
@@ -21,7 +34,8 @@ const Page = ({params}) => {
             <li>Phone number : {user.phone}</li>
             <li>Email : {user.email}</li>
             </ul>
-        </>
+            <ToastContainer/>
+        </div>
     )
 }
 export default Page
